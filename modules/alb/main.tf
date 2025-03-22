e6f6d5ccea9980dac4166dae045cef8e4024c6cb
kazi khalid <kazikhalid757@gmail.com>
2025-03-22 19:06:05 +0600

    update

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -3,6 +3,6 @@ module "alb" {
 
   name     = "private-alb"
   internal = true
-  subnets  = module.vpc.private_subnets  # Correct reference to VPC
-  vpc_id   = module.vpc.vpc_id           # Correct reference to VPC
+  subnets  = var.subnets  
+  vpc_id   = var.vpc_id           
 }
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,8 +3,8 @@ module "eks" {
   cluster_name    = "private-eks"
   cluster_version = "1.28"
 
-  subnet_ids = module.vpc.private_subnets  # Reference the correct VPC module
-  vpc_id     = module.vpc.vpc_id           # Reference the correct VPC module
+  subnet_ids = var.subnet_ids
+  vpc_id     = var.vpc_id      
   enable_irsa = true
 
   eks_managed_node_groups = {
@@ -13,7 +13,7 @@ module "eks" {
       min_size       = 2
       max_size       = 5
       desired_size   = 2
-      subnet_ids     = module.vpc.private_subnets # Reference correctly
+      subnet_ids     = var.subnet_ids
     }
   }
 }
