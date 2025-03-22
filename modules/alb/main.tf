e809d61a962ff4be8b8341a6ac5a93594331ed7f
kazi khalid <kazikhalid757@gmail.com>
2025-03-22 18:55:50 +0600

    update main

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,8 +1,8 @@
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name = "private-alb"  # Change "internal-alb" to "private-alb"
+  name     = "private-alb"
   internal = true
-  subnets  = module.vpc.private_subnets
-  vpc_id   = module.vpc.vpc_id
+  subnets  = module.vpc.private_subnets  # Correct reference to VPC
+  vpc_id   = module.vpc.vpc_id           # Correct reference to VPC
 }
--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,7 +1,7 @@
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  value = module.vpc.private_subnets
 }
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
