46603fa0e39f27d7a7d6ccbb582e5e86f2df0958
kazi khalid <kazikhalid757@gmail.com>
2025-03-22 22:53:55 +0600

    update

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "private_subnet_cidrs" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-east-2a", "us-east-2b"]
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "cluster_name" {
