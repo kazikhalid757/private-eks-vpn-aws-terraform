a75cafae3aecc3d70a06b5a99cb56bff2fddef48
kazi khalid <kazikhalid757@gmail.com>
2025-03-24 20:03:37 +0600

    update

--- modules/vpn/variables.tf
+++ modules/vpn/variables.tf
@@ -8,11 +8,6 @@ variable "private_subnets" {
   type        = list(string)
 }
 
-variable "vpn_client_cidr" {
-  description = "CIDR block for VPN clients"
-  type        = string
-  default     = "10.1.0.0/16"
-}
 
 variable "vpn_cert_domain" {
   description = "Domain name for the VPN certificate"
