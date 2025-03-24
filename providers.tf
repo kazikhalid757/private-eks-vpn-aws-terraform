d9ce1eb70a170e8dadc3a652ce731eb29aa0e462
kazi khalid <kazikhalid757@gmail.com>
2025-03-24 18:08:36 +0600

    update

--- providers.tf
+++ providers.tf
@@ -9,9 +9,9 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_name
+# }
 
 # Helm Provider
 provider "helm" {
