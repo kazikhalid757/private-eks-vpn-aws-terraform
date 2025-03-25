c9b60d8a9cf7aae9e8dc78a577b565cd7744bee8
kazi khalid <kazikhalid757@gmail.com>
2025-03-25 19:54:57 +0600

    update

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,10 +1,10 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.id
+# }
 
-output "private_subnets" {
-  value = aws_subnet.private[*].id
-}
+# output "private_subnets" {
+#   value = aws_subnet.private[*].id
+# }
 
 output "security_group_id" {
   value = aws_security_group.all_worker_mgmt.id
--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.eks_cluster_endpoint
-}
-
-output "eks_cluster_name" {
-  value = module.eks.eks_cluste_name
-}
-output "eks_cluster_id" {
-  value = module.eks.eks_cluster_id
-}
-
-output "alb_dns_name" {
-  value = module.alb.alb_url
-}
-
-# output "nginx_ingress_dns" {
-#   value = module.dns.nginx_ingress_dns
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
 # }
+
+# output "eks_cluster_endpoint" {
+#   value = module.eks.eks_cluster_endpoint
+# }
+
+# output "eks_cluster_name" {
+#   value = module.eks.eks_cluste_name
+# }
+# output "eks_cluster_id" {
+#   value = module.eks.eks_cluster_id
+# }
+
+# output "alb_dns_name" {
+#   value = module.alb.alb_url
+# }
+
+# # output "nginx_ingress_dns" {
+# #   value = module.dns.nginx_ingress_dns
+# # }
