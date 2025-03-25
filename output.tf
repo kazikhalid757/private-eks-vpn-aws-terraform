5bdb0ae2da1912b12c642cb09d090fc2a2d2dc9a
kazi khalid <kazikhalid757@gmail.com>
2025-03-25 21:15:55 +0600

    update

--- output.tf
+++ output.tf
@@ -6,9 +6,9 @@ output "eks_cluster_endpoint" {
   value = module.eks.eks_cluster_endpoint
 }
 
-output "eks_cluster_name" {
-  value = module.eks.eks_cluste_name
-}
+# output "eks_cluster_name" {
+#   value = module.eks.eks_cluste_name
+# }
 output "eks_cluster_id" {
   value = module.eks.eks_cluster_id
 }
