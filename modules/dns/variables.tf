8f4800f6c731cf80e8102cec159e2fa441b8849b
kazi khalid <kazikhalid757@gmail.com>
2025-03-28 22:02:28 +0600

    update

--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -1,3 +1,19 @@
-variable "vpc_id" {}
-variable "domain_name" {}
-# variable "alb_dns" {}
+variable "domain_name" {
+  description = "The domain name for the VPN"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the DNS will be hosted"
+  type        = string
+}
+
+variable "vpn_cert_domain_validation_options" {
+  description = "Domain validation options from VPN module"
+  type        = map(any)
+}
+
+variable "vpn_dns_name" {
+  description = "The DNS name of the VPN endpoint"
+  type        = string
+}
--- modules/vpn/output.tf
+++ modules/vpn/output.tf
@@ -1,15 +1,9 @@
-output "vpn_endpoint_id" {
-  description = "The ID of the AWS Client VPN endpoint"
-  value       = aws_ec2_client_vpn_endpoint.vpn.id
+output "vpn_cert_domain_validation_options" {
+  description = "Domain validation options for ACM certificate"
+  value       = aws_acm_certificate.vpn_cert.domain_validation_options
 }
 
-output "vpn_endpoint_dns_name" {
-  description = "The DNS name of the VPN endpoint"
+output "vpn_dns_name" {
+  description = "The DNS name for the VPN endpoint"
   value       = aws_ec2_client_vpn_endpoint.vpn.dns_name
 }
-
-output "vpn_configuration_file" {
-  description = "Command to export the OpenVPN configuration file"
-  value       = "aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.vpn.id} --output text > vpn-config.ovpn"
-}
-
